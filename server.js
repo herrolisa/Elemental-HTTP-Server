ebe0f4296936d6cf16eabefe42eeaeeb7ada1e52
Lisa <misslisazhou@gmail.com>
2016-07-06 21:30:02 -1000

    able to create html with data from POST method

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var http = require('http');
+var querystring = require('querystring');
 var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
-  console.log(request.headers);
-
+var server = http.createServer(function (request, response) {
   var method = request.method;
 
   var uriRequest = request.url;
@@ -11,18 +10,48 @@ var server = http.createServer(function(request, response) {
     uriRequest = '/index.html';
   }
 
-  var dataBuffer = '';
+  //concatenate incoming POST data into a string
+  var completePost = '';
   request.on('data', function (data) {
-    console.log(data);
-    dataBuffer += data;
+    completePost += data;
   });
 
+  //do this when data finishes coming in
   request.on('end', function () {
     if (method ==='POST'){
-      console.log(dataBuffer);
+      //convert concatenated string into object
+      var postObject = querystring.parse(completePost);
+      var bodyHTML = '';
+      var i = 1;
+      //seperate keys and values in object, extract values and add header tags
+      for (var key in postObject){
+        bodyHTML += '<h' + i + '>' + postObject[key] + '</h' + i + '>';
+        i++;
+      }
+
+      //write html boilerplate to string
+      var fullHTML = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>The Elements - ' + postObject.elementName + '</title><link rel="stylesheet" href="./public/css/styles.css"></head><body>' + bodyHTML + '</body></html>';
+
+      //create new HTML file with POST data
+      fs.writeFile('./public/' + postObject.elementName.toLowerCase() + '.html', fullHTML, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+      });
     }
   });
 
+  // fs.readFile('./public' + uriRequest, 'utf8', function (err, responseBody) {
+  //   if (err) {
+  //     uriRequest = '/404.html';
+  //     fs.readFile('./public' + uriRequest, 'utf8', function (err, responseBody) {
+  //       response.writeHead(404, {"Content-Type": "text/html"});
+  //       response.end(responseBody.toString());
+  //     });
+  //   }else if (method === 'GET'){
+  //     response.writeHead(200, {"Content-Type": "text/html"});
+  //     response.end(responseBody.toString());
+  //   }
+  // });
 });
 
 server.listen(8080);
